bbc72226c02964cd3915be21b0c6f949b124d58d
mantzaris <avmantzaris@gmail.com>
2025-03-12 21:00:20 -0400

    a start on the landing page and a global.css setup with a background color

--- renderer/src/App.svelte
+++ renderer/src/App.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
   import { Router, createRouter } from '@roxi/routify'
   import routes from '../.routify/routes.default.js'
-
   export const router = createRouter({ routes })
+
+  import { Styles } from '@sveltestrap/sveltestrap';
+
 </script>
 
+<!-- need? -->
+<Styles/> 
+
 <Router {router} />
+
---
+++ renderer/src/lib/ipc.ts
@@ -0,0 +1,10 @@
+import type { CONTEXT_BRIDGE } from "../../../electron/preload";
+
+declare global {
+  interface Window {
+    bridge: typeof CONTEXT_BRIDGE;
+  }
+}
+
+const ipc = window.bridge;
+export default ipc;
