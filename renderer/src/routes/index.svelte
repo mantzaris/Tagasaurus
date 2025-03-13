09417766636c93bdb7864f0104abd29d185aa8a9
mantzaris <avmantzaris@gmail.com>
2025-03-12 21:00:20 -0400

    a start on the landing page and a global.css setup with a background color

--- renderer/src/routes/index.svelte
+++ renderer/src/routes/index.svelte
@@ -1,39 +1,73 @@
 
-<script lang="ts">
-    import ipc from "../ipc";
-  
-  
-    let electronVersion: string = $state("");
-    let nodeVersion: string = $state("");
-  
-    let nodeVersionTxt = $derived(nodeVersion ? `Node: ${nodeVersion}` : "Node Version");
-    let electronVersionTxt = $derived(
-      electronVersion ? `Electron: ${electronVersion}` : "Electron Version",
-    );
-  
-    async function getVersion(opt: "electron" | "node") {
-      const version = await ipc.getVersion(opt);
-  
-      if (opt === "electron") {
-        electronVersion = version;
-      } else {
-        nodeVersion = version;
-      }
-    }
+<script lang="ts"> 
+    // import { Container } from '@sveltestrap/sveltestrap';
+    import { Container, Row, Col, Button, Card, CardBody, CardTitle, CardText, Navbar, NavbarBrand, Modal, ModalHeader, ModalBody, ModalFooter } from '@sveltestrap/sveltestrap';
+
   </script>
   
-  <main>
-  
-    <h1>Electron + Svelte</h1>
-  
+  <!-- <main style="background-color: green;">
+ 
     <i class="fa-solid fa-house"></i>
 
-    <div class="buttons container">
-      <button class="btn btn-primary" onclick={() => getVersion("node")}>{nodeVersionTxt}</button>
-      <button>Foo</button>
-      <button class="btn btn-primary" onclick={() => getVersion("electron")}>{electronVersionTxt}</button>
-    </div>
+    <a href="/main-menu">main</a>
+ 
+    </main> -->
 
-    <a href="/main-menu">Go to Main Menu</a>
-  </main>
+
+<Navbar color="dark" dark expand="md">
+  <NavbarBrand href="#">Tagasaurus</NavbarBrand>
+</Navbar>
+
+
+    <Container class="mt-4">
+      <Row>
+        <Col md="4">
+          <Card>
+            <CardBody>
+              <CardTitle >Import Files</CardTitle>
+              <CardText>Upload images for annotation.</CardText>
+              <Button color="primary" >Open</Button>
+            </CardBody>
+          </Card>
+        </Col>
+        <Col md="4">
+          <Card>
+            <CardBody>
+              <CardTitle >Describe Images</CardTitle>
+              <CardText>Tag and describe your images.</CardText>
+              <Button color="primary" >Open</Button>
+            </CardBody>
+          </Card>
+        </Col>
+        <Col md="4">
+          <Card>
+            <CardBody>
+              <CardTitle >Export / Search</CardTitle>
+              <CardText>Find and export your annotations.</CardText>
+              <Button color="primary" >Open</Button>
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+      <Row class="mt-4">
+        <Col md="6">
+          <Card>
+            <CardBody>
+              <CardTitle >Webcam Analysis</CardTitle>
+              <CardText>Analyze live images from your webcam.</CardText>
+              <Button color="primary" >Open</Button>
+            </CardBody>
+          </Card>
+        </Col>
+        <Col md="6">
+          <Card>
+            <CardBody>
+              <CardTitle >Settings</CardTitle>
+              <CardText>Configure application preferences.</CardText>
+              <Button color="primary" >Open</Button>
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   
