6ae8f96d558982579c6be686910e3b371af1e3b2
mantzaris <avmantzaris@gmail.com>
2025-03-15 14:24:05 -0400

    now a component for the gallery view to display the image but not yet for the edit view

--- renderer/vite.config.ts
+++ renderer/vite.config.ts
@@ -3,11 +3,19 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 import routify from '@roxi/routify/vite-plugin' // import RoutifyPlugin from "@roxi/routify/vite-plugin";
 
+import * as path from 'path';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte(), routify()],
   base: "./", // Use relative paths for electron
   build: {
     outDir: "../dist/renderer/",
+  },
+  resolve: {
+    alias: {
+      // This line sets up the $lib alias
+      $lib: path.resolve(__dirname, 'src/lib'),
+    },
   },
 });
