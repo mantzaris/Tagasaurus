ffd9eda13615dcf6630391b03126e81162c0cf8c
mantzaris <avmantzaris@gmail.com>
2025-03-23 21:43:30 -0400

    now the gallery search modal UI is set up for various screen sizes. the backend has not been done

--- renderer/src/routes/tagging/[slug].svelte
+++ renderer/src/routes/tagging/[slug].svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { Container, Row, Col, Button, Input, Icon, Image } from '@sveltestrap/sveltestrap';
+  import { Container, Row, Col, Button, Input, Icon, Image, Modal, ModalBody, ModalHeader, ModalFooter, Accordion, AccordionItem, Figure } from '@sveltestrap/sveltestrap';
   import { params, goto } from '@roxi/routify';
 
   import MediaView from '$lib/MediaView.svelte';
@@ -12,10 +12,47 @@
 
     let description = "hi";
     let mode: "edit" | "gallery" = "edit";
+    
+    let openSearch = false;
+    const toggleSearch = () => {
+      openSearch = !openSearch
+      console.log(`open search = ${openSearch}`)
+      };
+
+
+
+    let searchInputId = 1;
+    let accordionOpen = true;
+
+    const toggleSearchAccordion = (...args: any[]) => {
+      console.log('toggle', ...args);
+    };
+
+    let faces = [
+    { id: 1, src: 'https://picsum.photos/100/100?random=1', selected: false },
+    { id: 2, src: 'https://picsum.photos/300/100?random=2', selected: false },
+    { id: 3, src: 'https://picsum.photos/100/100?random=3', selected: false },
+    { id: 4, src: 'https://picsum.photos/100/300?random=3', selected: false },
+    { id: 5, src: 'https://picsum.photos/100/100?random=5', selected: false },
+    { id: 6, src: 'https://picsum.photos/200/150?random=4', selected: false },
+    { id: 7, src: 'https://picsum.photos/100/100?random=7', selected: false },
+    { id: 8, src: 'https://picsum.photos/100/100?random=8', selected: false },
+    { id: 9, src: 'https://picsum.photos/100/100?random=9', selected: false },
+    { id: 10, src: 'https://picsum.photos/500/300?random=11', selected: false },
+    { id: 11, src: 'https://picsum.photos/100/100?random=11', selected: false }
+  ];
+
+  function toggleFace(i: number) {
+      faces[i] = {
+      ...faces[i],
+      selected: !faces[i].selected
+    };
+    faces = [...faces];
+  }
 </script>
 
 
-<Container  fluid class="d-flex flex-column vh-100 p-0 m-0 .min-vh-0" style="min-height: 0;" >
+<Container  fluid class="d-flex flex-column vh-100 p-2 m-0 .min-vh-0" style="min-height: 0;" >
   <!-- <MyComponent name="Tester" /> -->
   <!-- Layout for extra-small screens -->
   <div class="d-block d-sm-none">
@@ -38,7 +75,7 @@
     <Row class="mb-2">
       <Col class="d-flex justify-content-center gap-2">
         <Button color="primary" size="sm"><Icon name="caret-left-fill" class="fs-4"/></Button>
-        <Button color="primary" size="sm"><Icon name="search" class="fs-4"/></Button>
+        <Button color="primary" size="sm" on:click={toggleSearch}><Icon name="search" class="fs-4"/></Button>
         <Button color="primary" size="sm"><Icon name="caret-right-fill" class="fs-4"/></Button>
       </Col>
     </Row>
@@ -59,7 +96,7 @@
       </Col>
       <Col xs="4" class="d-flex justify-content-center gap-3">
         <Button color="primary" size="md"><Icon name="caret-left-fill" class="fs-3"/></Button>
-        <Button color="primary" size="md"><Icon name="search" class="fs-3"/></Button>
+        <Button color="primary" size="md" on:click={toggleSearch}><Icon name="search" class="fs-3"/></Button>
         <Button color="primary" size="md"><Icon name="caret-right-fill" class="fs-3"/></Button>
       </Col>
       <Col xs="4" class="d-flex justify-content-end">
@@ -88,7 +125,114 @@
     </div>
   {/if}
 
+  <Modal isOpen={openSearch} toggle={toggleSearch} scrollable size={"xl"}>
+    <!-- <ModalHeader toggle={toggleSearch}>Modal title</ModalHeader> -->
+    
+    <ModalBody>
+
+      <Accordion on:toggle={toggleSearchAccordion} class="mb-2">
+        <AccordionItem
+          active
+          header="Text Search"
+          on:toggle={(e) => {
+            searchInputId = 1;
+            accordionOpen = e.detail;
+          }}
+        >
+          <Input type="search" placeholder="enter text..." />
+        </AccordionItem>
+        <AccordionItem
+          header="Current Faces"
+          on:toggle={(e) => {
+            searchInputId = 2;
+            accordionOpen = e.detail;
+          }}
+        >
+          <div class="image-container">
+            {#each faces as face, i}
+              <!-- svelte-ignore a11y_click_events_have_key_events -->
+              <!-- svelte-ignore a11y_no_static_element_interactions -->
+              <div
+                class="image-item"
+                on:click={() => toggleFace(i)}
+              >
+                <Image class=" {face.selected ? 'border border-primary border-3' : ''}" thumbnail alt="Face Thumbnail" src={face.src} style="max-height: 15vh;"/>
+              </div>
+            {/each}
+          </div>
+        </AccordionItem>
+      </Accordion>
+
+      <Container fluid class="h-100" >
+        <Row class="g-0">
+          {#each faces as face}
+            <Col lg="6" class="d-flex justify-content-center align-items-center p-2">
+              <img class="img-fluid face-cell " src={face.src} alt="face" style="max-height: 100%; object-fit: contain; cursor: pointer;" />
+            </Col>
+          {/each}
+        </Row>
+      </Container>
+      
+    </ModalBody>
+    <ModalFooter >
+      <Button color="primary" on:click={toggleSearch} class="me-4"><Icon name="search" class="fs-3 "/></Button>
+      <Button color="secondary" on:click={toggleSearch} class="ms-4 me-2"><Icon name="x-square-fill" class="fs-3"/></Button>
+    </ModalFooter>
+  </Modal>
 
 </Container>
 
 
+<style>
+  .image-container {
+    display: flex;
+    flex-wrap: nowrap;
+    overflow-x: auto;
+    padding: 1rem;
+  }
+  .image-item {
+    flex: 0 0 auto;
+    margin-right: 1rem;
+    cursor: pointer;
+    border: 2px solid transparent;
+  }
+
+  .face-cell {
+    height: 20vh !important;
+  }
+
+  @media (max-width: 1000px) {
+    .face-cell {
+      height: 25vh !important;
+    }
+  }
+
+  @media (max-height: 768px) {
+    .face-cell {
+      height: 20vh !important;
+    }
+  }
+
+  @media (max-height:550px) {
+    .face-cell {
+      height: 15vh !important;
+    }
+  }
+</style>
+
+
+<!--      <Container  fluid class="d-flex flex-column h-100 .min-h-0" style="min-height: 0; background-color:red" >
+        {#each faces as face, i}
+          <!~~ svelte-ignore a11y_click_events_have_key_events ~~>
+          <!~~ svelte-ignore a11y_no_static_element_interactions ~~>
+            <div class="tt p-2">
+              <img class="zz" alt="Landscape" src={face.src}  />
+            </div>          
+        {/each}
+      </Container>-->
+<!-- <div class="image-item {face.selected ? 'border border-primary border-3' : ''}"> -->
+  <!-- <Figure caption="I believe this is a cow needing a haircut" class="tt">
+    <Image fluid alt="Landscape" src={face.src}  />
+  </Figure> -->
+<!-- <Image fluid alt="This is a fluid Image" src={face.src} class="tt"/> -->
+<!-- </div> -->
